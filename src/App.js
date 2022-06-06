12c377eef290a748c2a1c9a8a851d3312719fc1b
Nelly Mariana Hernandez Marquez <mrboohdz@gmail.com>
2022-06-05 19:59:05 -0500

    Fotos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+
+  const [fotos, setFotos] = useState([]);
+  const [tabla, setTabla] = useState(<></>);
+
+  const actualizarFotos = async () => {
+    let url = 'https://jsonplaceholder.typicode.com/photos';
+    let res = await axios({
+      url,
+      method: 'GET',
+    });
+    console.log(res.data);
+    setFotos(res.data);
+    setTabla(
+      fotos.map((data) => (
+        <div className='col-md-3 my-1' key={data.id}>
+          <img src={data.url} width="100%"></img> 
+        </div>
+      ))
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <input type="button" className="btn btn-primary" value="Fotos" onClick={actualizarFotos}></input>
+          </div>
+        </div>
+        <div className='col-md-12'>
+          <div className='row' id='resultados'>
+            {
+              tabla
+            }
+          </div>
+          
+        </div>
+      </div>
+      
     </div>
   );
 }
